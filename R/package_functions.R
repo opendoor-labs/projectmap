176b0c0154867e8058528ea1fd7f6f290a5c475b
Alex Hubbard <alex.hubbard@opendoor.com>
2018-07-05 15:38:03 -0700

    Upated app environment

--- R/package_functions.R
+++ R/package_functions.R
@@ -566,7 +566,7 @@ link_to_proj = function(init = F, app = F){
     }
   }else{
     set_proj_lib(app = T)
-    exit_proj(reset_lib = F)
+    #exit_proj(reset_lib = F)
     message("App environment set.")
   }
 }
