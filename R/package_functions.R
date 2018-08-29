cb1184cd89199b1a2f9ae9fc1bca0425810a4888
Alex Hubbard <alex.hubbard@opendoor.com>
2018-08-29 16:36:49 -0700

    update to render_file

--- R/package_functions.R
+++ R/package_functions.R
@@ -1216,7 +1216,7 @@ render_file = function(file, inFolder = NULL, quiet = T, clean = T, ...){
   proj.env$file = get_file_path(file, inFolder)
   proj.env$current.dir = dirname(proj.env$file)
   lock_proj()
-  pacman::p_load(get_proj_packages(files = file, parallel = F), character.only = T, install = F)
+  pacman::p_load(get_proj_packages(files = proj.env$file, parallel = F), character.only = T, install = F)
   rmarkdown::render(get_file_path(file, inFolder = inFolder),
                     quiet = quiet, clean = clean, knit_root_dir = proj.env$root.dir,
                     output_dir = get_output_dir(doc = T))
