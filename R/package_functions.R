89543fc5e74cd7a0173181960555c431924bf43c
alexhubbardOD <alex.hubbard@opendoor.com>
2019-03-28 10:00:54 -0700

    changed line and point width in theme

--- R/package_functions.R
+++ R/package_functions.R
@@ -1462,12 +1462,12 @@ od_theme = function(palette = "main", discrete = T, reverse = F, addblack = F,
   }else{
     stop("Error defining color palette")
   }
-  update_geom_defaults("line", list(size = 1.175))
-  update_geom_defaults("abline", list(size = 1.175))
-  update_geom_defaults("vline", list(size = 1.175))
-  update_geom_defaults("hline", list(size = 1.175))
-  update_geom_defaults("segment", list(size = 1.175))
-  update_geom_defaults("point", list(size = 1.25))
+  update_geom_defaults("line", list(size = 1.125))
+  update_geom_defaults("abline", list(size = 1.125))
+  update_geom_defaults("vline", list(size = 1.125))
+  update_geom_defaults("hline", list(size = 1.125))
+  update_geom_defaults("segment", list(size = 1.125))
+  update_geom_defaults("point", list(size = 1.2))
   return(od.theme)
 }
 
