f8acd94ec263cd1bb3e0d4847d14c911d87fa59f
Alex Hubbard <alex.hubbard@opendoor.com>
2019-06-27 16:00:44 -0700

    testing

--- R/package_functions.R
+++ R/package_functions.R
@@ -730,7 +730,7 @@ link_to_proj = function(init = F, install = T){
             }
           })
           names(version_check) = installed_packages$Package
-          cat(paste(version_check, collapse = "\n"))
+          assign("version_check", version_check, .GlobalEnv)
           if(any(version_check == T)){
             warning("Installed versions of ", paste(names(version_check[!is.na(version_check)][version_check[!is.na(version_check)] == T]), collapse = ", "), " do not match the required version.\n\nUpdate ./Functions/required_packages.csv or install the required versions.")
           }
