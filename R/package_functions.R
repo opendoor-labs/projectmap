e4d6bb4dce2f7f4a5f00049bf44e584143fd0909
Alex Hubbard <alex.hubbard@opendoor.com>
2019-07-17 12:36:12 -0700

    a

--- R/package_functions.R
+++ R/package_functions.R
@@ -358,13 +358,7 @@ reset_proj_env = function(build = F, newroot = F){
 get_proj_env = function(){
   if(file.exists(".proj_env.RData")){
     load(".proj_env.RData")
-    if(bindingIsActive("proj.env", parent.frame())){
-      if(bindingIsLocked("proj.env", parent.frame())){
-        unlockBinding("proj.env", parent.frame())
-      }
-    }
     assign("proj.env", proj.env, parent.frame())
-    lockBinding("proj.env", parent.frame())
   }else{
     return(NULL)
   }
