e155aeb7dc4b6b0a421ca483c250752919bcfee4
Alex Hubbard <alex.hubbard@opendoor.com>
2019-07-17 14:40:28 -0700

    a

--- R/package_functions.R
+++ R/package_functions.R
@@ -318,7 +318,7 @@ reset_proj_env = function(build = F, newroot = F){
 #' @export
 get_proj_env = function(...){
   if(file.exists(".proj_env.RData")){
-    args = list(..)
+    args = list(...)
     assign("proj.env", load(".proj_env.RData"))
     if(length(list) == 0){
       return(proj.env)
